c44f967a5a88cf5cbab27f677d7f1ab4ff39b92d
shingo-suzumura <shingo.msdf@gmail.com>
2021-04-16 13:29:04 +0000

    課題７まで終了

---
+++ gyakuten-salon/resources/views/posts/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts')
+
+@section('content')
+
+    
+    <div class="container mt-4">
+        <div class="border p-4">
+            <h1 class="h5 mb-4">
+                投稿の編集
+            </h1>
+            
+            <form method="POST" action="{{route('posts.update',['post'=>$post])}}">
+                {{csrf_field()}}
+                {{method_field('PUT')}}
+                
+                <fielset class="mb-4">
+                    <div class="form-group">
+                        <label for="title">
+                            タイトル
+                        </label>
+                        <input 
+                            id="title"
+                            name="title"
+                            type="text" 
+                            class="form-control"
+                            value="{{$post->title}}"
+                        >
+                    </div>
+                    
+                    <div>
+                        <label for="body">
+                            本文
+                        </label>
+                        <textarea
+                            id="body"
+                            name="body"
+                            rows="4"
+                            class="form-control"
+                        >
+                            {{$post->body}}
+                        </textarea>
+                    </div>
+                    
+                    <div class="mt-5">
+                        <a  class="btn btn-secondary" href="{{route('posts.show',['post'=>$post])}}">
+                            キャンセル
+                        </a>
+                        
+                        <button class="btn btn-primary" type="submit">
+                            更新する
+                        </button>
+                    </div>
+                    
+                </fielset>
+            </form>
+        </div>
+    </div>
+    
+    
+
+@endsection('content')
